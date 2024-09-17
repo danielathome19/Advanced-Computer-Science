702b5806d5dfec7a09747b94afcdc3f0a830eb50
Daniel J. Szelogowski <dszelogowski@gmail.com>
2024-09-17 15:29:59 +0000

    Update

--- src/Q1/Prog460a.java
+++ src/Q1/Prog460a.java
@@ -2,6 +2,7 @@ package Q1;
 import java.io.*;
 import java.util.*;
 import Utils.Console;
+import Algorithms.SearchAlgorithms;
 
 public class Prog460a {
     public static void main(String[] args) {
@@ -12,24 +13,17 @@ public class Prog460a {
                 nums.add(file.nextInt());
             file.close();
             
+            int lcv = 0;
+            var arr = new Integer[nums.size()];
+            for (var n : nums) arr[lcv++] = n;
+
             int num = 0;
             do {
                 num = Console.input("Enter a number to search for: ");
-                int pos = binarySearch(nums, num);
+                int pos = SearchAlgorithms.binarySearch(arr, num);
                 System.out.println(pos == -1 ? "Your number does not occur in this list\n" 
                                              : "Your number occurs at position " + (pos + 1) + "\n");
             } while (num != -1);
         } catch (IOException e) { System.out.println("File error"); }
-    }
-
-    public static int binarySearch(ArrayList<Integer> nums, int num) {
-        int low = 0, high = nums.size() - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (nums.get(mid) == num) return mid;
-            else if (nums.get(mid) < num) low = mid + 1;
-            else high = mid - 1;
-        }
-        return -1;
     }
 }
