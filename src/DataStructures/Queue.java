6d9e3b39397b2bccd79d1c143e4a6d73f67cd716
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-10-22 12:06:36 -0500

    Update 10/22

---
+++ src/DataStructures/Queue.java
@@ -0,0 +1,49 @@
+package DataStructures;
+
+@SuppressWarnings("unchecked")
+public class Queue<T extends Comparable<T>> {
+    // Protected means "private except to child classes"
+    protected T[] queue;
+    protected int front;
+    protected int rear;
+    protected int size;
+    protected int capacity;
+
+    public Queue() {
+        size  = 0;
+        front = 0;
+        rear  = 0;
+        capacity = 1;
+        queue = (T[]) new Comparable[capacity];
+    }
+
+    public void enqueue(T element) {
+        if (size == capacity) resize(2 * capacity);
+        queue[rear] = element;
+        rear = (rear + 1) % capacity;
+        size++;
+    }
+
+    public T dequeue() {
+        T element = queue[front];
+        front = (front + 1) % capacity;
+        size--;
+        if (size == capacity / 4) resize(capacity / 2);
+        return element;
+    }
+
+    public T peek()          { return queue[front]; }
+    public int size()        { return size; }
+    public boolean isEmpty() { return size == 0; }
+
+    protected void resize(int newCapacity) {
+        if (newCapacity < 1) newCapacity = 1;
+        T[] newQueue = (T[]) new Comparable[newCapacity];
+        for (int i = 0; i < size; i++)
+            newQueue[i] = queue[(front + i) % capacity];
+        queue = newQueue;
+        capacity = newCapacity;
+        front = 0;
+        rear = size;
+    }
+}
